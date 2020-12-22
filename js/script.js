f2f1ad07b94d62a7c83db7727cdfd3f0d202a675
Gianpaolo <pao.barga@gmail.com>
2020-12-22 12:32:59 +0100

    cambiato struttura label: label contiene tutto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-// variabili generali
-// ?come chiamo queste variabili (come gruppo) nei commenti?
+/* DA FARE 
+_confronta versione pushata da Ottavio e vedi se puoi semplificare qlcsa
+_cambia for con indexOf
+_html: togli ingredient-container? (e modifica selettore js, nel caso)
+*/
+
+// costanti dell'applicazione
 var couponList = ['abcd', 'bcda', 'dbca', 'dddd'];
+var burgerBaseCost = 14;
+var discount = 20;
+
+// contatori e flag
 var total = 0;
 
 // selezione input
@@ -12,12 +21,13 @@ var price = document.getElementById('price');
 
 // quando submit viene attivato
 button.addEventListener('click', function(e) {
-    e.preventDefault();
+    // e.preventDefault();
 
     // raccogli value di name e controlla che ci siano solo lettere (e che quindi non sia vuoto)
     if (/[a-z]/ig.test(burgerName.value)) {
 
         // raccogli in un arr gli ingredienti selezionati
+        // !potevi utilizzare una variabile counter invece dell'array!     
         var checkedIngredients = [];
         for(var i = 0; i < ingredientList.length; i++) {
             if(ingredientList[i].checked) {
@@ -28,25 +38,26 @@ button.addEventListener('click', function(e) {
         // controlla che siano selezionati almeno due ingredienti
         if(checkedIngredients.length >= 2) {
             // reset total
-            total = 0;
+            total = burgerBaseCost;
             for(var l = 0; l < checkedIngredients.length; l++) {
                 total += parseInt(checkedIngredients[l].value);
             }
 
             // controlla se coupon è presente
-            if (coupon.value !== '') {
+            if (coupon.value !== '') { // * if separati così da dare errore solo se coupon sbagliato
                 var check = false; // flag per avviso coupon non valido
                 
                 // controlla se coupon è valido (in lista)
+                // !potevi usare un semplice indexOf!
                 for(var m = 0; m < couponList.length; m++) {
                     if(coupon.value === couponList[m]) {
                         check = true;
-                        total -= (total/100*20);
+                        total -= (total/100*discount);
                     }
                 }
 
                 // se coupon non è valido
-                if (!check) {
+                if (!check) { // * if separati così da dare errore solo se coupon sbagliato
                     alert('Coupon non valido')
                 }
             } 
